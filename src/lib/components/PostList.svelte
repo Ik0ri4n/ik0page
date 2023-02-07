2c1198762640266f9c1c3df85eeb6ad8066df7a2
Ik0ri4n <48494824+Ik0ri4n@users.noreply.github.com>
2023-02-07 20:27:05 +0100

    Improve styling of post list

---
+++ src/lib/components/PostList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PostInfo } from '$lib/types/post';
+	import PostPreview from './PostPreview.svelte';
+
+	export let posts: PostInfo[];
+</script>
+
+<ul class="mt-3 mb-6 space-y-2 sm:mb-8 sm:mt-6 sm:space-y-4">
+	{#each posts as post}
+		<li>
+			<PostPreview {post} />
+		</li>
+	{/each}
+</ul>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import PostList from '$lib/components/PostList.svelte';
 	import PostPreview from '$lib/components/PostPreview.svelte';
 	import type { PageData } from './$types';
 
@@ -8,11 +9,5 @@
 <article class="mx-auto max-w-3xl">
 	<h1 class="text-2xl font-medium text-gray-900 dark:text-gray-50 sm:text-3xl">Posts</h1>
 
-	<ul class="mt-2 space-y-2 sm:mt-4 sm:space-y-4">
-		{#each data.posts as post}
-			<li>
-				<PostPreview {post} />
-			</li>
-		{/each}
-	</ul>
+	<PostList posts={data.posts} />
 </article>
