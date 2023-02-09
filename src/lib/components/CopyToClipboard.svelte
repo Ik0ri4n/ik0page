49d5449aee0628e0dde5db309fdca879335aa57b
Ik0ri4n <48494824+Ik0ri4n@users.noreply.github.com>
2023-02-09 21:37:29 +0100

    Feature/dice ctf post (#3)
    Write complete blog post about two DiceCTF 2023 challenges.
    Additionally, implement a new challenge card component.

---
+++ src/lib/components/CopyToClipboard.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let text: string;
+	const copy = () => {
+		navigator.clipboard.writeText(text);
+	};
+</script>
+
+<button on:click={copy} title="Click to copy">{text}</button>
